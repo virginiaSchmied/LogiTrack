e1fd5ada5c53d8749b468334f9a569b811d67566
virginiaSchmied <schmiedvirginia@gmail.com>
2026-04-03 23:00:08 -0300

    fix(ci.yml): Corregir codigo para job test

--- src/backend/database.py
+++ src/backend/database.py
@@ -5,7 +5,7 @@ import os
 
 load_dotenv()
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
